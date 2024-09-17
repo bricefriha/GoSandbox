82835ce674b14ed0ea0bcaadea145af6a5ce74e5
John Doe <contact@bricefriha.dev>
2024-09-17 22:24:55 +0100

    Chapter 1 | step 12

--- src/main.go
+++ src/main.go
@@ -1,32 +1,34 @@
-// Including the main package 
-package main;
+// Including the main package
+package main
 
-// Importing fmt 
-import ( 
+// Importing fmt
+import (
 	"GoLangSanbox/src/chapterOne"
-    "fmt"
-) 
+	"fmt"
+)
 
 func main() {
-	var i, j int = 1,2
-	k :=3 ;
+	var i, j int = 1, 2
+	k := 3
 	c, python, java := true, false, "no!"
 	// step 1
-	chapterOne.RandomNum();
+	chapterOne.RandomNum()
 	// step 2
-	chapterOne.SquareSeven();
+	chapterOne.SquareSeven()
 	// step 3
-	chapterOne.PrintPi();
+	chapterOne.PrintPi()
 	// step 4 and 5
-	chapterOne.PrintAddition(3,5);
+	chapterOne.PrintAddition(3, 5)
 	// step 6
-	chapterOne.PrintSwap("Sky","Blue");
+	chapterOne.PrintSwap("Sky", "Blue")
 	// step 7
-	chapterOne.PrintSplit(17);
+	chapterOne.PrintSplit(17)
 	// step 8 and 9
-	chapterOne.PrintArguments(c, python, java);
+	chapterOne.PrintArguments(c, python, java)
 	// Step 10
-	chapterOne.Print4Arguments(i, j, k, c, python, java);
-	fmt.Print("\n---------");
-	fmt.Print("\n Finished");
+	chapterOne.Print4Arguments(i, j, k, c, python, java)
+	// Step 12
+	chapterOne.SquareRoot(-5 + 12i)
+	fmt.Print("\n---------")
+	fmt.Print("\n Finished")
 }
